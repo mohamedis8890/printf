689eda3db151b2baa7b95703c5ff3f858c043783
Hossam Gomah <hossamgomah4@gmail.com>
2023-10-17 07:04:32 -0700

    including the library and struct

--- main.h
+++ main.h
@@ -1,5 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} match;
 
 int _printf(const char *format, ...);
 
